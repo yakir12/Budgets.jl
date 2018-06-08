6cc140f9db6420d3630ca0d9a90730f9b88fd5ef
yakir12 <12.yakir@gmail.com>
2018-06-08 14:53:56 +0200

    better coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,19 +33,12 @@ end
 end
 
 @testset "in" begin 
-    events = ["e: 1-1-1, e1, 12, 44.4", "d: yea, -44.4", "p: 2014-11-11, -44.4"]
+    events = ["e: , e1, 1, 44.4", "e: 1-1-1, e1, 11, 44.4", "d: yea, -44.4", "p: 2014-11-11, -44.4"]
     b = Budget(Percentile(14))
     add!(b, define_event.(events)...)
     @test balance(b) == Money(12*44.4*1.14-44.4-44.4)
 end
 
 @testset "out" begin
-    @test print(b) == "January 1, 1 & e1 & 12 & 44.4 sek & 532.8 sek \\\\\n\\hline\n & Total & & & 532.8 sek\\\\\n & VAT (14\\%) & & &74.59 sek\\\\\n & yea & & & -44.4 sek\\\\\nNovember 11, 2014 & Payment & & & -44.4 sek\\\\\n& Balance due & & & 518.59 sek\\\\"
-end
-
-@testset "missing" begin
-    e4 = "e: , e1, 2, 50"
-    b = Budget(Percentile(10))
-    add!(b, define_event(e4))
-    @test balance(b) == Money{Float64}(110)
+    @test print(b) == "January 1, 1 & e1 & 11 & 44.4 sek & 488.4 sek \\\\\n & e1 & 1 & 44.4 sek & 44.4 sek \\\\\n\\hline\n & Total & & & 532.8 sek\\\\\n & VAT (14\\%) & & &74.59 sek\\\\\n & yea & & & -44.4 sek\\\\\nNovember 11, 2014 & Payment & & & -44.4 sek\\\\\n& Balance due & & & 518.59 sek\\\\"
 end
