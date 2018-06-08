80c0e1c3812ab8a9519de5146373620cc726d771
yakir12 <12.yakir@gmail.com>
2018-06-08 13:39:50 +0200

    coverage

--- src/Budgets.jl
+++ src/Budgets.jl
@@ -17,11 +17,9 @@ struct Money{T<:Real}
 end
 
 *(m::Money, x::Int) = Money(m.value*x)
-*(x::Int, m::Money) = m*x
 *(p::Percentile, x::Money) = Money(p.p/100*x.value)
 *(x::Money, p::Percentile) = p*x
 +(m::Money, x::Money) = Money(m.value + x.value)
--(m::Money, x::Money) = Money(m.value - x.value)
 
 abstract type Event end
 
@@ -149,130 +147,6 @@ function print(b::Budget)
 
     append!(o, "& Balance due & & & ", my_format(balance(b)), "\\\\")
     return String(o)
-    end
+end
 
-
-    #=function printmd(b::Budget)
-    nexpenses = length(b.expenses)
-    ndeductions = length(b.deductions)
-    npayments = length(b.payments)
-    a = ["" for i = 1:nexpenses + ndeductions + 4 + npayments, j = 1:4]
-    a[1,:] = ["Description", "Units", "Rate", "Amount"]
-    for (r, expense) in enumerate(b.expenses), (c, field) in enumerate([:description, :units, :price, :sum])
-    a[r + 1, c] = my_format(getfield(expense, field))
-    end
-    a[nexpenses + 2,3] = "Total"
-    a[nexpenses + 2,4] = my_format(b.sumb4tax)
-    a[nexpenses + 3,3] = "VAT ($(my_format(b.moms.p))%)"
-    a[nexpenses + 3,4] = my_format(b.tax)
-    for (r, deduction) in enumerate(b.deductions)
-    a[r + nexpenses + 3,3] = deduction.description
-    a[r + nexpenses + 3,4] = my_format(deduction.sum)
-    end
-    for (r, payment) in enumerate(b.payments)
-    a[r + nexpenses + 3 + ndeductions,3] = my_format(payment.date)
-    a[r + nexpenses + 3 + ndeductions,4] = my_format(payment.sum)
-    end
-    a[nexpenses + ndeductions + npayments + 4, 3] = "Balance due"
-    a[nexpenses + ndeductions + npayments + 4, 4] = my_format(b.sum)
-    return print(GridTable, a)
-    end=#
-
-    end # module
-
-    #=b = Budget(Percentile(0))
-    x1 = Expense("kaka", 33, 45)
-    x2 = Expense("kaki", 3, 45.5)
-    x3 = Expense("pipi", 1, 45.5)
-    d = Deduction("why not", -100)
-    p1 = Payment(now(), -20)
-    p2 = Payment(Date(1999,1,22), -50)
-    add!(b, x1)
-    add!(b, x2)
-    add!(b, x3)
-    add!(b, d)
-    add!(b, p1)
-    add!(b, p2)
-    budget2array(b)
-
-
-
-    for c in ["Item ", "Units ", "Price (SEK) ", "Amount (SEK) "]
-    print(aa, c)
-    end
-    println(aa)
-    for expense in b.expenses
-    for field in [:description, :units, :price, :sum]
-    print(aa, getfield(expense, field))
-    end
-    println(aa)
-    end
-    print(STDOUT, aa)
-
-    function write(b::Budget; receipt=false)
-    nexpenses = length(b.expenses)
-    ndeductions = length(b.deductions)
-    a = Any["" for i = 1:nexpenses + ndeductions + 4, j = 1:4]
-    a[1,:] = ["Item", "Units", "Price (SEK)", "Amount (SEK)"]
-    for j = 1:nexpenses
-    a[j + 1,1] = b.expenses[j].description
-    a[j + 1,2] = b.expenses[j].units
-    a[j + 1,3] = b.expenses[j].price
-    \begin{longtabu} to \textwidth { l X c r r }
-        \hline
-        \blue{Date} & \blue{Description} & \blue{Units} & \blue{Rate} & \blue{Amount} \\
-        \hline
-        a[j + 1,4] = b.expenses[j].sum
-        end
-        a[nexpenses + 2,1] = "Fees"
-        a[nexpenses + 2,4] = b.sumb4tax
-        a[nexpenses + 3,1] = "VAT ($(b.moms)%)"
-        a[nexpenses + 3,4] = b.tax
-        for j = 1:ndeductions
-        a[j + nexpenses + 3,1] = b.deductions[j].description
-        a[j + nexpenses + 3,4] = b.deductions[j].sum
-        end
-        a[end, 1] = "Total"
-        a[end, 4] = b.sum
-        if receipt
-        a[end,1] *= " owed"
-        a = cat(1, a, ["Total payed" "" "" b.sum; "Balance" "" "" RoundReal(0)])
-        end
-        return printMD(a)
-        end
-
-        write(x::Expense) = "$(x.description) & $(x.units) & $(x.price) & $(x.sum)\\\\"
-
-        write(x::Deduction) = "$(x.description) & & & $(-x.sum)\\\\"
-
-        function write(b::Budget)
-        txt = ["""\\begin{longtable}{l c c r}
-            Item & Units & Price (SEK) & Amount (SEK)\\\\
-            \\hline"""]
-            append!(txt, write.(b.expenses))
-            push!(txt, """\\hline
-            Fees & & & $(b.sumb4tax)\\\\
-            VAT ($(b.moms)\\%) & & & $(b.tax)\\\\""")
-            if length(b.deductions) > 0
-            append!(txt, write.(b.deductions))
-            end
-            push!(txt, """\\hline
-            \\hline
-            \\bf Total & & & \\bf $(b.sum)\\\\
-        \\end{longtable}""")
-        return join(txt, "\n")
-        end
-
-        using StringBuilders
-        import StringBuilders:append!
-        function append!(sb::StringBuilder, ss::AbstractString...)
-        for s in ss
-        append!(sb, s)
-        end
-        end
-
-        o = StringBuilder()
-        append!(o, "2")
-        String(o)
-        append!(o, "3", "4", "5")
-        =#
+end # module
